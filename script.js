ddc6fc951b93afc2953182d7a1c9eb370857c388
Henrik005 <henrik.mathias.brathen@gmail.com>
2024-10-04 14:16:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 //Model
 let app = document.getElementById('App')
-let trueFalse = true;
-let runCounter = ''
-let num1 = 0
-let num2 = 0
-
+let noMatch = true;
+let runCounter = 0;
+let randomNum1 = 0;
+let randomNum2 = 0;
 
 
 
@@ -12,7 +11,7 @@ let num2 = 0
 //View
 updateView();
 function updateView(){
-app.innerHTML = /*HTML*/`<div>How Many Tries Until Numbers Matched: ${runCounter}</div> <div>The Numbers: ${num1}/${num2}<div>
+app.innerHTML = /*HTML*/`<div>How Many Tries Until Numbers Matched: ${runCounter}</div> <div>The Numbers: ${randomNum1}/${randomNum2}<div>
  <button onclick ="matchOrNot()">Run Match Program!</button>
 
 `
@@ -27,17 +26,26 @@ app.innerHTML = /*HTML*/`<div>How Many Tries Until Numbers Matched: ${runCounter
 
 //Controller
 function matchOrNot(){
-    while(trueFalse){
-     num1 = Math.floor(Math.random() * 50 + 1);
-     num2 = Math.floor(Math.random() * 50 + 1);
-    if(num1 != num2){runCounter++}
-    else if(num1 == num2){trueFalse = false;}
+    randomNumGen();
+    reset();
+    while(noMatch){
+     randomNum1 = randomNumGen();
+     randomNum2 = randomNumGen();
+    if(randomNum1 != randomNum2){runCounter++}
+    else{noMatch = false;}
     }
     updateView();
 }
 
-
-
+function randomNumGen(){
+    return Math.floor(Math.random() * 50 + 1);
+}
+function reset(){
+    runCounter = 0
+    randomNum1 = 0
+    randomNum2 = 0
+    noMatch = true;
+}
 
 
 
